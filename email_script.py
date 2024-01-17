8b3f526984f5a25da6baab0039bc0062c0e5d0b2
Eliahu Ben Amotz <97426320+Eliahu551818@users.noreply.github.com>
2024-01-17 13:58:03 +0200

    Ready to publish

--- email_script.py
+++ email_script.py
@@ -7,9 +7,9 @@ import smtplib
 from email.message import EmailMessage
 
 # change these as per use
-your_name = "Beit Mashiach LA"
-your_email = "beitmashiachla@gmail.com"
-your_password = "nddszshajvwmymmo"
+your_name = "My Name"
+your_email = "myemail@gmail.com"
+your_password = "My_Password_123"
 
 # Python code to send email to a list of
 # emails from a spreadsheet
@@ -20,13 +20,14 @@ server.ehlo()
 server.login(your_email, your_password)
 
 # reading the spreadsheet
-email_list = pd.read_excel('/Users/eliahubenamotz/Downloads/coding/python/hi.xlsx')
+email_list = pd.read_excel('emails_list.xlsx')
 
 # getting the names and the emails
 names = email_list['name']
 emails = email_list['email']
 
-
+subject = """my subject"""
+content = ("content").encode('utf8')
 
 # iterate through the records
 for i in range(len(emails)):
@@ -39,9 +40,8 @@ for i in range(len(emails)):
 	message = (
     f"From: {your_name} <{your_email}>\n"
     f"To: {name} <{email}>\n"
-    """Subject: טיסה לרבי - י' שבט תשפ"ג\n\n"""
-    f"""\nשלום וברכה\nלכבוד {name}\n\nאנחנו שמחים ונרגשים להודיע לכם על טיסה המאורגנת שלנו ל770 - בית משיח\nבשעה טובה, מתוכנן לו"ז כדלקמן\n- יוצאים ביום רביעי בשעה 12, אחרי שחרית וארוחת בוקר\n- מגיעים לשם בשעה 9 בערב ומתוודעים עד 3 בבוקר\n- הולכים לישון\n- קמים למקווה ושחרית עם המלך\n- ארוחת בוקר כיד המלך\n- טיסת חזור בשעה 2\n\nבאהבה, הרב מנדי ורוחמה דיין - בית משיח לוס-אנג'לס""").encode('utf8')
-
+    f"""Subject: {subject}"""
+	content
 	# sending the email
 	server.sendmail(your_email, [email], message)
 
